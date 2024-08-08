14c91667e0837f957874e9d7c9ba8a51805cea67
Umuttasan00umut.tasan24@gmail.com <umut.tasan24@gmail.com>
2024-08-08 13:28:19 +0300

    Initial Commit

---
+++ TradeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeApp
+//
+//  Created by Muhammet Umut Taşan on 8.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
